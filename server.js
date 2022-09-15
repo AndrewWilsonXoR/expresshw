702486adab9c8013cc10dc84760f9eb825cc4748
AndrewWilsonXoR <110188732+AndrewWilsonXoR@users.noreply.github.com>
2022-09-15 16:28:47 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -2,49 +2,20 @@ const express = require('express')
 const app = express()
 const ejs = require('ejs')
 
-const answers = 
-["It is certain",
-"It is decidedly so",
-"Without a doubt",
-"Yes definitely",
-"You may rely on it",
-"As I see it yes",
-"Most likely",
-"Outlook good",
-"Yes",
-"Signs point to yes",
-"Reply hazy try again",
-"Ask again later",
-"Better not tell you now",
-"Cannot predict now",
-"Concentrate and ask again",
-"Don't count on it",
-"My reply is no",
-"My sources say no",
-"Outlook not so good",
-"Very doubtful"]
+app.set('view engine', 'ejs');
 
-app.get('/greeting', (req,res) =>{
-    res.send('Hello Stranger')
+app.get('/', (req,res) =>{
+    res.send(`<h1> 99 Bottles of beer on the wall </h1>` + `<h1>99 Bottles of beer</h1>` + `<h1><a href="/98">ya take one down, pass it around</a></h1>`)
 })
-app.get('/greeting/:name', (req,res) =>{
-    res.send('Wow! Hello there, ' + req.params.name)
+app.get('/:bottle', (req, res)=>{
+    const minus = parseInt(req.params.bottle)
+    const newBottles = (minus - 1).toString()
+    res.send(
+        `<h1>`+ req.params.bottle +  `Bottles of beer on the wall </h1>`
+        + `<h1>`+ req.params.bottle +  `Bottles of beer</h1>`
+        + `<h1><a href="/` + newBottles + `">ya take one down, pass it around</a></h1>` 
+        + `<h1>` + newBottles +  `Bottles of beer on the wall.</h1>`)
 })
-app.get('/tip/:total/:tipPercentage', (req,res) =>{
-
-    let total = parseInt(req.params.total);
-    let tip = parseInt(req.params.tipPercentage);
-    let tipTotal =(total*(tip/100)).toString()
-    res.send(tipTotal)
-})
-app.get('/magic', (req,res) =>{
-    res.send('Ask a question in the URL')
-})
-app.get('/magic/:question', (req,res) =>{
-    let randomAnswer = answers[Math.floor(Math.random()*answers.length)]
-    res.send(req.params.question + "? " + randomAnswer)
-})
-
 app.listen(3000, (req,res)=>{
-    console.log("listening on 3000");
+    console.log('listening')
 })
